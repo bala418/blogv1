6f934f43aa929dbbebbf41402b8be362e97808d9
bala418 <balavtwo@gmail.com>
2021-09-16 20:52:49 +0530

    try changing port

--- app.js
+++ app.js
@@ -55,6 +55,6 @@ app.post("/", function (req, res) {
 	res.redirect("/");
 });
 
-app.listen(3000 || process.env.PORT, function () {
-	console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, function () {
+	console.log("Server is running on port 3000");
 });

04f9a805bdba3ee54457b040957a7c5a675f3df5
bala418 <balavtwo@gmail.com>
2021-09-16 20:45:16 +0530

    edit for hosting on heroku

--- app.js
+++ app.js
@@ -55,6 +55,6 @@ app.post("/", function (req, res) {
 	res.redirect("/");
 });
 
-app.listen(3000, function () {
+app.listen(3000 || process.env.PORT, function () {
 	console.log("Server started on port 3000");
 });

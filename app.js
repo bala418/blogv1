abdfc551135e15532c2cec24713d179bf337e433
bala418 <balavtwo@gmail.com>
2022-03-01 23:03:30 +0530

    Add mongodb connection to local environment

--- app.js
+++ app.js
@@ -1,60 +1,83 @@
-
 const express = require("express");
 // const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent =
+  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent =
+  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent =
+  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+const mongoose = require("mongoose");
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 // app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/blogsDB");
 
-var posts = [];
+const blogSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+});
 
+const Blog = mongoose.model("Blog", blogSchema);
 
 app.get("/", function (req, res) {
-	res.render("home", { hs: homeStartingContent, postss: posts });
+  Blog.find({}, (err, items) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("home", { postss: items, hs: homeStartingContent });
+    }
+  });
 });
 
 app.get("/about", function (req, res) {
-	res.render("about", { as: aboutContent });
+  res.render("about", { as: aboutContent });
 });
 
 app.get("/contact", function (req, res) {
-	res.render("contact", { cs: contactContent });
+  res.render("contact", { cs: contactContent });
 });
 
 app.get("/compose", function (req, res) {
-	res.render("compose");
+  res.render("compose");
 });
 
 app.get("/posts/:postName", function (req, res) {
-	var reqTitle = req.params.postName;
-	posts.forEach(function (post) {
-		var storedTitle = post.title;
-		var x = reqTitle.replace('-', ' ').toLowerCase();
-		var y = storedTitle.toLowerCase();
-		if (x === y) {
-			res.render("post", { title: storedTitle, content: post.body });
-		}
-	});
+  var reqID = req.params.postName;
+  console.log(reqID);
+  Blog.findOne({ _id: reqID }, (err, item) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("post", { post: item });
+    }
+  });
 });
 
 app.post("/", function (req, res) {
-	var post = {
-		title: req.body.postTitle,
-		body: req.body.postContent
-	};
-	posts.push(post);
-	res.redirect("/");
+  console.log(req.body);
+  const post = new Blog({
+    title: req.body.postTitle,
+    content: req.body.postContent,
+  });
+
+  console.log(post);
+
+  post.save((err) => {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
+
+  res.redirect("/");
 });
 
 app.listen(process.env.PORT || 3000, function () {
-	console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000");
 });

eadb3146a4eba704d1999a23fa02b7eae16848ec
bala418 <balavtwo@gmail.com>
2022-08-04 20:42:25 +0530

    Fix errors

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 // const bodyParser = require("body-parser");
 const ejs = require("ejs");
-require("dotenv").config();
+
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent =
@@ -17,7 +18,7 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost:27017/blogDB");
+mongoose.connect(process.env.MONGO_URI);
 
 const blogSchema = new mongoose.Schema({
   title: String,
